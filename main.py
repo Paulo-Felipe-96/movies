9e3b990ffc6ed495e008d0e71b88ad9d8b927d53
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2021-11-02 01:23:56 -0300

    Initial setup for main to run the server

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 """ initial state """
+from config.parameters import HTTP_PORT, LOCAL_HOST
+from uvicorn import Server, Config
+
+server = Server(Config("service.server:app", host=LOCAL_HOST, port=HTTP_PORT))
+
+if __name__ == '__main__':
+    server.run()
